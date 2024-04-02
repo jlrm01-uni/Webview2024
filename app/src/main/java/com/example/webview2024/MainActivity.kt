f72f858dc9ed183f105eb2d452e3e3cf62b88ec3
jlrm01-uni <jorge.rodriguez@guayama.inter.edu>
2024-04-02 09:52:00 -0400

    added buttons that take me to different websites

--- app/src/main/java/com/example/webview2024/MainActivity.kt
+++ app/src/main/java/com/example/webview2024/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.webview2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // This is a git test
+        val webView = findViewById<WebView>(R.id.webView)
+
+        webView.loadUrl("https://godotengine.org")
+        webView.settings.javaScriptEnabled = true
+
+        val viewWebsiteButton = findViewById<Button>(R.id.visitWebsiteButton)
+
+        viewWebsiteButton.setOnClickListener {
+            webView.loadUrl("https://www.python.org")
+        }
+
+        val moreInfoButton = findViewById<Button>(R.id.moreInfoButton)
+
+        moreInfoButton.setOnClickListener {
+            webView.loadUrl("https://guayama.inter.edu")
+        }
+
     }
 }
